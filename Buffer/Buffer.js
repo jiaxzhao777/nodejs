6fd55ad34f09091130f67b83ca263be23a25587f
jiaxzhao777 <jiaxzhao@ebay.com>
2024-04-24 23:41:53 +0800

    feat: remove node demo

--- Buffer/Buffer.js
+++ Buffer/Buffer.js
@@ -1,4 +1,6 @@
 const b1 = Buffer.from("10");
 const b2 = Buffer.from("笨");
+const b3 = Buffer.from("圆圆子");
 console.log(b1);
 console.log(b2);
+console.log(b3);
--- node-demo/server/VDom.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VDom = () => {
-  return <div>我是一个被渲染为真实DOM的虚拟DOM</div>;
-};
-
-export default VDom;
