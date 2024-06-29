ecbc0fe3cd5edbed58e1f82d0c398f8a0fafea5e
jiaxzhao777 <jiaxzhao@ebay.com>
2024-06-29 10:28:57 +0800

    feat: stream

--- Buffer/Buffer.js
+++ Buffer/Buffer.js
@@ -8,3 +8,20 @@ console.log(b1);
 console.log(b2);
 console.log(b3);
 console.log(b4);
+
+const buf = Buffer.from("Hello, world!");
+// 转为字符串输出
+console.log(buf.toString()); // 输出 'Hello, world!'
+
+// 转为16进制字符串输出
+console.log(buf.toString("hex")); // 输出 '48656c6c6f2c20776f726c6421'（对应的是 'Hello, world!' 的 ASCII 码）
+
+// 转为数组输出
+console.log(Array.from(buf)); // 输出 [
+//    72, 101, 108, 108, 111,
+//    44,  32, 119, 111, 114,
+//   108, 100,  33
+// ]
+
+// 转为base64格式输出
+console.log(buf.toString("base64")); // 输出 'SGVsbG8sIHdvcmxkIQ=='
---
+++ Module/module.js
@@ -0,0 +1,9 @@
+console.log(__filename);
+console.log(__dirname);
+console.log(process.argv);
+console.log(process.cwd());
+// console.log(process.env);
+process.stdout.write("hello");
+process.stdout.write(" ");
+process.stdout.write("world");
+process.stdout.write("\n");
